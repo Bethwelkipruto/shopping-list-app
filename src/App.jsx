d75a209357c74e4ff427fbbc623a981553f48d27
bethwelkipruto <bethwel.c7@gmail.com>
2025-07-29 22:37:54 +0300

    Fix Footer file case sensitivity for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,6 @@ function App() {
               />
             }
           />
-          {}
           <Route path="/add" element={<AddItemForm onAddItemSuccess={handleAddItem} />} />
           <Route path="/about" element={<About />} />
         </Routes>
